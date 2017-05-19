1dd840cb24f82e4ee753af9210e0cf7c7a3f341c
jangjongkyu <wkdwhdrb1212@naver.com>
2017-05-19 17:56:21 +0900

    바꿈
    바꿈

--- src/test/QuizTable.java
+++ src/test/QuizTable.java
@@ -3,4 +3,5 @@ package test;
 public class QuizTable {
 	public static String[] quiz = new String[]{"��ī��","������","���̸�","������","����Ǯ","�ߵ���","������","�ǰ���"};
   /*깃허브 테스트*/
+	/*깃 테스트 2*/
 }
